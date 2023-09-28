e44bf86c82f02f0b0bce368556f320f190a86d06
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-09-28 23:04:24 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import { Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes} from "react-router-dom";
 import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
+	const user = localStorage.getItem("token");
+
   return (
     <Routes>
+      {user && <Route path="/" exact element={<Home />} />}
       <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate replace to="/login" />} />
     </Routes>
   );
 }
